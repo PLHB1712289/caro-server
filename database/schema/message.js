7926bc09f1112cef3cc4738d3f55817400ab9a59
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2021-01-12 16:16:28 +0700

    change message model

--- database/schema/message.js
+++ database/schema/message.js
@@ -4,6 +4,7 @@ module.exports = mongoose.model(
   "message",
   new mongoose.Schema({
     idRoom: String,
+    idGame: { type: String, default: null },
     idUser: String,
     message: String,
     created_at: { type: Date, default: Date.now },
